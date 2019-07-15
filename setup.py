98aad9ac612f7467b9e5b876c934fc6192874acc
Sampath Herga <sampath@taskmonk.ai>
2019-07-15 16:53:20 +0530

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,21 +4,20 @@ from setuptools import setup, find_packages
 setup(
   name = 'taskmonk-sdk',         # How you named your package folder (MyLib)
   packages = ['taskmonk', 'taskmonk.utils'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '0.8',      # Start with a small number and increase it with every change you make
   license='apache-2.0',        # Chose a license from here: 
   description = 'SDK for accessing TaskMonk functionality',   # Give a short description about your library
   author = 'Taskmonk',                   # Type in your name
   author_email = 'info@taskmonk.com',      # Type in your E-Mail
   url = 'https://github.com/taskmonk/taskmonk-sdk-python',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/taskmonk/taskmonk-sdk-python/archive/v_1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/taskmonk/taskmonk-sdk-python/archive/v_0.8.tar.gz',    
   keywords = ['TaskMonk', 'SDK'],   # Keywords that define your package best
   install_requires=[            
   	'requests'
       ],
   classifiers=[
-    'Development Status :: 5 - Production',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      
-    'Topic :: Software Development :: APIs',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
